23286290bc70d7017e9f0dc9677eeffd90043f1c
David Cully <dc@davidcully.com>
2022-04-22 14:45:46 -0700

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ const Home = () => {
     <div className='card p-3 m-3'>
       <div className='card-body'>
         <h5>
-          Welcome to the public page for TimeKeeper. Please log in to
-          continue...
+          Welcome to the public page for TimeKeeper. Please{' '}
+          <a href='/login'>login here</a> to continue...
         </h5>
       </div>
     </div>
